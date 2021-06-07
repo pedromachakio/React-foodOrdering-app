f05cf03844d5492c43b47d80fb5eb9ffd9326167
Pedro Batista <pedromachakio@hotmail.com>
2021-06-07 13:36:26 +0000

    made the button really reusable by allowing its type to be set in the component it is used later on (but also adding a default fallback type)

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,7 +1,11 @@
 import styles from "./Button.module.css";
 
 const Button = (props) => {
-  return <button type="submit" className={styles.button}>Add User</button>;
+  return (
+    <button type={props.type || "button"} className={styles.button}>
+      Add User
+    </button>
+  );
 };
 
 export default Button;
