60199dd851ae5807f60e287bdb11501e537172c0
Pedro Batista <pedromachakio@hotmail.com>
2021-06-11 20:13:46 +0000

    added Portals to the Error Modal and the Backdrop so they aren't displayed deeply nested in the DOM but rather side by side with the root component since they overlap all the other components

--- src/Components/Modals/InvalidInput.js
+++ src/Components/Modals/InvalidInput.js
@@ -1,27 +1,48 @@
 import styles from "./InvalidInput.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onBackgroundClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.modalTitle}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.modalMessage}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onOkButtonClick}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
+
 
 const InvalidInput = (props) => {
-  const closeModalHandler = () => {
-    props.onOkClick();
-  };
 
   return (
-    <div>
-      <div className={styles.backdrop} onClick={closeModalHandler}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.errorTitle}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.errorMessage}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={closeModalHandler}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onBackgroundClick={props.onOkClick} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          modalTitle={props.errorTitle}
+          modalMessage={props.errorMessage}
+          onOkButtonClick={props.onOkClick}
+        />,
+        document.getElementById("overlay")
+      )}
+    </Fragment>
   );
 };
 
