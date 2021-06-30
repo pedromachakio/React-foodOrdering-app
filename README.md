40a28083ebe6eebb05bbb7508032bdf407a9c9b3
Pedro Batista <pedromachakio@hotmail.com>
2021-06-30 22:35:30 +0000

    initial commit of Create React App skeleton + README

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # REACT-APP-ADD-USERS-&-AGE
 
-This is a basic REACT app to add users and their age to an unordered list.
-
-It is being created as an answer to a challenge from a Udemy course to put what I learned thus far into practice.
+Food ordering app created with REACT.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import React, { useState, Fragment } from "react";
-import AddUser from "./Components/Users/AddUser";
-import UsersList from "./Components/Users/UsersList";
-
 function App() {
-  const [listOfUsers, setListOfUsers] = useState([]);
-
-  const addUserHandler = (userInfo) => {
-    setListOfUsers((previousListOfUsers) => {
-      return [
-        ...previousListOfUsers,
-        { key: userInfo.key, name: userInfo.name, age: userInfo.age },
-      ];
-    });
-  };
-
   return (
-    <Fragment>
-      <AddUser onUserAdded={addUserHandler} />
-      <UsersList users={listOfUsers} />
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
