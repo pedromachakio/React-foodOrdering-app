0256260e3cf9a015eadcb2094607038144215a7d
Pedro Batista <pedromachakio@hotmail.com>
2021-07-01 17:41:04 +0000

    Implemented a button to the Header component that will keep track of the amount of items the user currently has in the cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import styles from "./Header.module.css";
 import tableMealsImage from "../../assets/tablemeals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={styles["main-image"]}>
         <img src={tableMealsImage} alt="Table full of food" />
