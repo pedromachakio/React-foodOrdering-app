2d4773daada8918de4636ab20c7e7db8e48c5b00
Pedro Batista <pedromachakio@hotmail.com>
2021-07-10 17:55:51 +0000

    implemented the useReducer hook that will manage the state of the items that will be added or removed from the cart (this adding/removing logic is still missing for now)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,40 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.item); // concat e não push porque o push altera o array original, o concat devolve um novo
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action.type === "REMOVE_ITEM") {
+  }
+  return defaultCartState;
+}; // fica fora do component porque não é suposto ser rerendered sempre que o component for reevaluated e tmb pq n precisa de dados nenhuns lá de dentro
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", payload: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", payload: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
