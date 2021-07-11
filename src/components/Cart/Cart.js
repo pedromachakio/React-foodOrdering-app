6c911fdd847b68f41a85d5e9b9ba2b8449e0bbdb
Pedro Batista <pedromachakio@hotmail.com>
2021-07-11 21:28:41 +0000

    implemented the logic to actually display the items being added to the cart once we click the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,31 @@
+import CartContext from "../../store/cart-context";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const cartHasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        ></CartItem>
       ))}
     </ul>
   );
@@ -15,11 +35,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>*Numero Hardcoded que vai ser alterado</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onCloseCart}>Close</button>
-        <button className={styles.button}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        {cartHasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
